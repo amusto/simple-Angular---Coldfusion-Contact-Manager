42cecb38e8c125a240518cf42697f9a9c8c84dec
Armando Musto <armando.musto@gmail.com>
2014-09-28 16:22:13 -0400

    Changed default page to be only html, was a cfm page.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 	// create the module and re-name it contactsApp
 	var contactsApp = angular.module('contactsApp', ['ngRoute']);
 
-
 	// configure our routes
 	contactsApp.config(function($routeProvider, $locationProvider) {
 		$routeProvider
@@ -35,10 +34,49 @@
         $scope.$location = $location;
         $scope.$routeParams = $routeParams;
 
+        /*$scope.contacts = [
+            {
+                "fullname": "Kasey Koch",
+                "email": "DKahle@in.net",
+                "phone": "1-689-674-937"
+            },
+            {
+                "fullname": "Effram Koehn",
+                "email": "NLeeman@mattis.com",
+                "phone": "816-800-8094"
+            },
+            {
+                "fullname": "Colleen Harkey",
+                "email": "EFrutos@tempor.com",
+                "phone": "513-643-2883"
+            },
+            {
+                "fullname": "Guy Craig",
+                "email": "PDelatorre@risus.net",
+                "phone": "106-311-0309"
+            },
+            {
+                "fullname": "Bernard Young",
+                "email": "BGibson@libero.net",
+                "phone": "428-700-7597"
+            },
+            {
+                "fullname": "Isabel Reviews",
+                "email": "IMortimore@et.com",
+                "phone": "278-884-7653"
+            },
+            {
+                "fullname": "Sukanya Alua",
+                "email": "IAbney@vestibulum.org",
+                "phone": "932-521-9966"
+            }
+        ];*/
+
         $scope.loadContacts = function(){
             console.log('loading');
 
-            $.getJSON('contacts.cfc?method=getContacts',{},function(data){
+            //$.getJSON('contacts.cfc?method=getContacts',{},function(data){
+            $.getJSON('http://blog.armandomusto.com/code_samples/coldfusion_angular_contact_manager/contacts.cfc?method=getContacts',{},function(data){
                 $scope.$apply(function(){
                     $scope.contacts = data;
                     console.log($scope.contacts);
@@ -48,7 +86,7 @@
 
         $scope.getContact = function(contactId){
             $scope.contactId = contactId;
-            $.getJSON('contacts.cfc?method=getContacts',{},function(data){
+            $.getJSON('http://blog.armandomusto.com/code_samples/coldfusion_angular_contact_manager/contacts.cfc?method=getContacts',{},function(data){
                 $scope.$apply(function(){
                     $scope.contacts = data;
                     $rootScope.contact = $scope.contacts[$scope.contactId];
@@ -72,8 +110,9 @@
             thisContact = $scope.contacts[index];
             thisContact.id = index;
 
-            $.post( "contacts.cfc?method=deleteContact", { id:thisContact.id }, function( data ) {
+            $.post( "http://blog.armandomusto.com/code_samples/coldfusion_angular_contact_manager/contacts.cfc?method=deleteContact", { id:thisContact.id }, function( data ) {
                 $scope.loadContacts();
+                $location.path("/");
              });
         };
 
@@ -103,13 +142,13 @@
 
             if(thisContact.exists==0){
                 console.log('Add contact');
-                $.post( "contacts.cfc?method=addContact", { id:thisContact.id, fullname:thisContact.fullname, email:thisContact.email, phone:thisContact.phone }, function( data ) {
+                $.post( "http://blog.armandomusto.com/code_samples/coldfusion_angular_contact_manager/contacts.cfc?method=addContact", { id:thisContact.id, fullname:thisContact.fullname, email:thisContact.email, phone:thisContact.phone }, function( data ) {
                     $scope.loadContacts();
                     $location.path("/");
                 })
             } else {
                 console.log('Updating contact');
-                $.post( "contacts.cfc?method=updateContact", { id:thisContact.id, fullname:thisContact.fullname, email:thisContact.email, phone:thisContact.phone }, function( data ) {
+                $.post( "http://blog.armandomusto.com/code_samples/coldfusion_angular_contact_manager/contacts.cfc?method=updateContact", { id:thisContact.id, fullname:thisContact.fullname, email:thisContact.email, phone:thisContact.phone }, function( data ) {
                     $scope.loadContacts();
                     $location.path("/");
                 })
